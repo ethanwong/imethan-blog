7f29df45dffaa49ffa0b09a1dafb7885adf148d6
Ethan Wong <ethanxm@gmail.com>
2014-03-08 13:10:19 +0800

    mongoDB添加密码配置

--- src/main/java/com/the3/web/cms/HelloWorldController.java
+++ src/main/java/com/the3/web/cms/HelloWorldController.java
@@ -1,8 +1,14 @@
 package com.the3.web.cms;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.the3.entity.cms.Channel;
+import com.the3.service.ChannelService;
 
 /**
  * HelloWorldController.java
@@ -12,11 +18,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HelloWorldController {
+	
+	@Autowired
+	private ChannelService channelService;
 
-    @RequestMapping("/helloWorld")
-    public String helloWorld(Model model) {
-    	System.out.println("111111111111");
+    @RequestMapping("/add")
+    public String channel(Model model) {
+    	System.out.println("HelloWorldController channel:"+new Date());
+        model.addAttribute("message", "add channel!");
+        
+        String title = ""+new Date();
+        Channel channel = new Channel();
+        channel.setTitle(title);
+        channel.setDescribe("这个是描述！");
+        channel.setCreateTime(new Date());
+        channelService.save(channel);
+        
+        return "helloWorld";
+    }
+    
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+    	System.out.println("HelloWorldController hello:"+new Date());
         model.addAttribute("message", "Hello World!");
+        
         return "helloWorld";
     }
 }
